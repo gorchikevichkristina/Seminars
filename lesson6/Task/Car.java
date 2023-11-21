7263c8103f24b18dbdf8a78c26b55e27e1133a5c
Kristina Gorchikevich <gorchikevich@bk.ru>
2023-11-21 19:58:36 +0300

    save 21/11/23 seminar OOP 6

---
+++ lesson6/Task/Car.java
@@ -0,0 +1,23 @@
+package lesson6.Task;
+
+class Car {
+    public void startEngine() {
+        // Запустить двигатель автомобиля с бензиновым двигателем
+    }
+}
+
+class ElectricCar {
+    public void startElectricMotor() {
+        // Запустить электрический двигатель автомобиля
+    }
+}
+
+class HybridCar {
+    public void startEngine() {
+        // Запустить бензиновый двигатель автомобиля
+    }
+
+    public void startElectricMotor() {
+        // Запустить электрический двигатель автомобиля
+    }
+}
